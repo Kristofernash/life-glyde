3e2cb86624b1cf7372013292224dea71151e5c3e
Mark Faulkner <markfaulk350@gmail.com>
2018-08-23 13:15:42 -0700

    working form with bootstrap styling

--- lifeglyde/client/src/components/Profileform.js
+++ lifeglyde/client/src/components/Profileform.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import API from "../utils/API";
 
 export default class Profileform extends Component {
-    
+
     state = {
         id: this.props.userId,
         firstName: '',
@@ -34,78 +34,120 @@ export default class Profileform extends Component {
     render() {
         return (
             <form>
-                <input
-                    name="firstName"
-                    placeholder='First Name'
-                    value={this.state.firstName}
-                    onChange={e => this.change(e)}>
-                </input>
-                <br />
-                <input
-                    name="lastName"
-                    placeholder='Last Name'
-                    value={this.state.lastName}
-                    onChange={e => this.change(e)}>
-                </input>
-                <br />
-                <input
-                    name="phoneNum"
-                    placeholder='Phone #'
-                    value={this.state.phoneNum}
-                    onChange={e => this.change(e)}>
-                </input>
-                <br />
-                <input
-                    name="profilePic"
-                    placeholder='Profile Picture URL:'
-                    value={this.state.profilePic}
-                    onChange={e => this.change(e)}>
-                </input>
-                <br />
-                <input
-                    name="glider"
-                    placeholder='Glider Make and Color/s'
-                    value={this.state.glider}
-                    onChange={e => this.change(e)}>
-                </input>
-                <br />
-                <input
-                    name="ushpaRating"
-                    placeholder='USHPA Rating'
-                    value={this.state.ushpaRating}
-                    onChange={e => this.change(e)}>
-                </input>
-                <br />
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <input
+                            name="firstName"
+                            placeholder='First Name'
+                            type="text"
+                            className="form-control"
+                            value={this.state.firstName}
+                            onChange={e => this.change(e)}>
+                        </input>
+                    </div>
+                    <br />
+                    <div className="form-group col-md-6">
+                        <input
+                            name="lastName"
+                            placeholder='Last Name'
+                            type="text"
+                            className="form-control"
+                            value={this.state.lastName}
+                            onChange={e => this.change(e)}>
+                        </input>
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <input
+                            name="phoneNum"
+                            placeholder='Phone #'
+                            type="text"
+                            className="form-control"
+                            value={this.state.phoneNum}
+                            onChange={e => this.change(e)}>
+                        </input>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <input
+                            name="profilePic"
+                            placeholder='Profile Picture URL:'
+                            type="text"
+                            className="form-control"
+                            value={this.state.profilePic}
+                            onChange={e => this.change(e)}>
+                        </input>
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <input
+                            name="glider"
+                            placeholder='Glider Make and Color/s'
+                            type="text"
+                            className="form-control"
+                            value={this.state.glider}
+                            onChange={e => this.change(e)}>
+                        </input>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <input
+                            name="ushpaRating"
+                            placeholder='USHPA Rating'
+                            type="text"
+                            className="form-control"
+                            value={this.state.ushpaRating}
+                            onChange={e => this.change(e)}>
+                        </input>
+                    </div>
+                </div>
+                <div className="form-row">
+                <div className="form-group col-md-12">
                 <input
                     name="medicalInfo"
                     placeholder='Pertinent Medical Info'
+                    type="text"
+                    className="form-control"
                     value={this.state.medicalInfo}
                     onChange={e => this.change(e)}>
                 </input>
-                <br />
+                </div>
+                </div>
+                <div className="form-row">
+                <div className="form-group col-md-4">
                 <input
                     name="emergencyContactName"
                     placeholder='Emergency Contact Name'
+                    type="text"
+                    className="form-control"
                     value={this.state.emergencyContactName}
                     onChange={e => this.change(e)}>
                 </input>
-                <br />
+                </div>
+                
+                <div className="form-group col-md-4">
                 <input
                     name="emergencyContactRelation"
                     placeholder='Emergency Contact Relation'
+                    type="text"
+                    className="form-control"
                     value={this.state.emergencyContactRelation}
                     onChange={e => this.change(e)}>
                 </input>
-                <br />
+                </div>
+                <div className="form-group col-md-4">
                 <input
                     name="emergencyContactPhone"
                     placeholder='Emergency Contact Phone #'
+                    type="text"
+                    className="form-control"
                     value={this.state.emergencyContactPhone}
                     onChange={e => this.change(e)}>
                 </input>
-                <br />
-                <button onClick={(e) => this.onSubmit(e)}>Submit</button>
-            </form>
+                </div>
+                </div>
+                <button type="button" className="btn btn-dark" onClick={(e) => this.onSubmit(e)}>Submit</button>
+            </form >
         )
     };
 }
